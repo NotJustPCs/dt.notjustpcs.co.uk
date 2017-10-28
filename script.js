ee920f40426d603110087123b30ada5d1d949de5
Peter Fossey <peter@fossweb.net>
2017-10-28 18:15:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ function WriteLinks(IDtoWrite) {
 	WLlinks = WLlinks + WriteOneLink("https://moz.com/researchtools/ose/links?site=","",document.getElementById('DomainName').value,"bar-chart","Domain Authority rank")
 	WLlinks = WLlinks + WriteOneLink("http://quirktools.com/screenfly/#u=http%3A//","&w=320&h=568&a=37",document.getElementById('DomainName').value,"tablet","Open website (as an iPhone 5)")
 	WLlinks = WLlinks + WriteOneLink("http://www.tcpiputils.com/domain-neighbors/","",document.getElementById('DomainName').value,"road","Domain Neighbours")
+	WLlinks = WLlinks + WriteOneLink("http://dnstrails.com/#/domain/domain/","",document.getElementById('DomainName').value,"history","DNS History")
+	WLlinks = WLlinks + WriteOneLink("https://who.is/whois/","",document.getElementById('DomainName').value,"user-secret","Whois")
+	WLlinks = WLlinks + WriteOneLink("https://mxtoolbox.com/archive/?actionstring=mx:","&run=networktools",document.getElementById('DomainName').value,"envelope","MX Toolbox")
+	WLlinks = WLlinks + WriteOneLink("https://mxtoolbox.com/archive/?actionstring=blacklist:","",document.getElementById('DomainName').value,"ban","Blacklist check (MX Toolbox)")
+	WLlinks = WLlinks + WriteOneLink("https://mxtoolbox.com/SuperTool.aspx?action=https%3a","&run=toolpage",document.getElementById('DomainName').value,"lock","MX Toolbox (SSL Check)")
+	WLlinks = WLlinks + WriteOneLink("http://toolbar.netcraft.com/site_report?url=","",document.getElementById('DomainName').value,"list","Netcraft Site Report")
+	WLlinks = WLlinks + WriteOneLink("http://web.archive.org/web/*/","",document.getElementById('DomainName').value,"clock-o","Wayback Machine")
+	WLlinks = WLlinks + WriteOneLink("https://sitecheck.sucuri.net/results/","",document.getElementById('DomainName').value,"bug","Sucuri Scan")
 	WLlinks = WLlinks + WriteOneLink("http://dt.notjustpcs.co.uk/?","",document.getElementById('DomainName').value,"link","Shortcut this page")
 	document.getElementById(IDtoWrite).innerHTML = WLlinks;
 }
